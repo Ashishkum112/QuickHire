72d7ba3dca4483624aa7dfee270327c88430c2f0
Ashishkum112 <ashishkumarr0214@gmail.com>
2024-08-11 01:58:34 +0530

    Created Login and Sign up page

---
+++ frontend/src/components/auth/Signup.jsx
@@ -0,0 +1,149 @@
+import React, { useState } from "react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import Navbar from "../shared/Navbar";
+import { Button } from "@/components/ui/button"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { USER_API_END_POINT } from "../../utils/constants";
+
+const Signup = () => {
+
+
+
+    const [input,setInput] = useState({
+        fullname:"",
+        email:"",
+        phoneNumber:"",
+        password:"",
+        role:"",
+        file:""
+      });
+    
+      const changeEventHandler = (e) =>{
+        setInput({...input,[e.target.name]:e.target.value});
+      }
+    
+      const changeFileHandler = (e)=>{
+        setInput({...input,file:e.target.files?.[0]});
+      }
+
+      const submitHandler = async(e) =>{
+        e.preventDefault();
+        const formData = new FormData;
+        formData.append("fullname", input.fullname);
+        formData.append("email", input.email);
+        formData.append("phoneNumber", input.phoneNumber);
+        formData.append("password", input.password);
+        formData.append("role", input.role);
+        if(input.file){
+            formData.append("file",input.file)
+        }
+        try {
+            const res = await axios.post(`${USER_API_END_POINT}/register`, formData , {
+                headers:{
+                    "Content-Type":"multipart/form-data"
+                },
+                withCredentials:true
+            })
+        } catch (error) {
+            console.log(error);
+            
+        }
+        
+      }
+
+  return (
+    <div>
+      <Navbar />
+      <div className="flex items-center justify-center max-w-7xl mx-auto">
+        <form
+          onSubmit={submitHandler}
+          className="w-1/2 border border-gray-200 rounded-md p-4 my-10"
+        >
+          <h1 className="font-bold text-xl mb-5">Sign Up</h1>
+          <div className="my-2">
+            <Label>Full Name</Label>
+            <Input 
+            type="text" 
+            value={input.fullname}
+            name="fullname"
+            onChange={changeEventHandler}
+            placeholder="Ashish" 
+            />
+          </div>
+          <div className="my-2">
+            <Label>Email</Label>
+            <Input 
+                type="email" 
+                value={input.email}
+                name="email"
+                onChange={changeEventHandler}
+                placeholder="ashish@gmail.com" 
+            />
+          </div>
+          <div className="my-2">
+            <Label>Phone Number</Label>
+            <Input 
+            type="text" 
+            value={input.phoneNumber}
+            name="phoneNumber"
+            onChange={changeEventHandler}
+            placeholder="9213423523" 
+            />
+          </div>
+          <div className="my-2">
+            <Label>Password</Label>
+            <Input 
+            type="password" 
+            value={input.password}
+            name="password"
+            onChange={changeEventHandler}
+            placeholder="password" 
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <RadioGroup className='flex items-center gap-4 my-4'>
+              <div className="flex items-center space-x-2">
+                <Input
+                type='radio'
+                name='role'
+                value='student'
+                checked={input.role === 'student'}
+                onChange={changeEventHandler}
+                className="cursor-pointer"
+                />
+                <Label htmlFor="r1">Student</Label>
+              </div>
+              <div className="flex items-center space-x-2">
+              <Input
+                type='radio'
+                name='role'
+                value='recruiter'
+                checked={input.role === 'recruiter'}
+                onChange={changeEventHandler}
+                className="cursor-pointer"
+                />
+                <Label htmlFor="r2">Recruiter</Label>
+              </div>
+            </RadioGroup> 
+            <div className="flex items-center gap-2">
+            <Label>Profile</Label>
+            <Input
+            accept='image/*'
+            type='file'
+            onChange={changeFileHandler}
+            className='cursor-pointer'
+            />
+            </div>
+          </div>
+        <Button type='submit' className='w-full my-4'>Signup</Button>
+        <span className="text-sm">Already have an account ? <Link to='/login' className='text-blue-600'>Login</Link></span>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -1,25 +1,75 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Popover } from "../ui/popover";
+import { Button } from "@/components/ui/button"
+import { PopoverContent, PopoverTrigger } from "@radix-ui/react-popover";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { LogOut, User2 } from "lucide-react";
 
 const Navbar = () => {
+  const user = false;
+
   return (
-    <div className='bg-white'>  
-    <div className='flex items-center justify-between mx-auto max-w-7xl'>
-    <div>
-      <h1 className='text-2xl font-bold'>Job<span className='text-[#F83002]'>Portal</span></h1>
-      </div>
-    <div>
-        <ul className='flex font-medium items-center gap-5'>
+    <div className="bg-white">
+      <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
+        <div>
+          <h1 className="text-2xl font-bold">
+            Job<span className="text-[#F83002]">Portal</span>
+          </h1>
+        </div>
+        <div className="flex items-center gap-12">
+          <ul className="flex font-medium items-center gap-5">
             <li>Home</li>
             <li>Jobs</li>
             <li>Browse</li>
-            
-        </ul>
-    </div>
-    </div>
+          </ul>
+          {
+            !user?(
+              <div className="flex items-center gap-2">
+                <Link to='/login'>
+                <Button variant ="outline">Login</Button>
+                </Link>
+                <Link to='/signup'>
+                <Button className='bg-[#6A38C2] hover:bg-[#4b18a4]'>Sign Up</Button>
+                </Link>
+              </div>
+            ):(
+              <Popover>
+            <PopoverTrigger asChild>
+              <Avatar className="cursor-pointer">
+                <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn"/>
+              </Avatar>
+            </PopoverTrigger>
+            <PopoverContent className="w-80">
+              <div className='flex gap-4 space-y-2'>
+                <Avatar className='cursor-pointer'>
+                  <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn"/>
+                </Avatar>
+                <div>
+                <h4 className='font-medium'>Ashish Kumar</h4>
+                <p className="text-sm text-muted-foreground">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus totam praesentium tempora alias voluptatibus voluptas tenetur </p>
+                </div>
+              </div>
+              <div className="flex flex-col my-2 text-gray-500">
+                <div className="flex w-fit items-center gap-2 cursor-pointer">
+                <User2/>
+                <Button variant="link">Profile</Button>
+                </div >
+                <div className="flex w-fit items-center gap-2 cursor-pointer">
+                <LogOut/>
+                <Button variant="link">Logout</Button>
+                </div>
 
+              </div>
+            </PopoverContent>
+          </Popover>
+            )
+          }
+          
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
