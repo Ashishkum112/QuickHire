08b6ac3cfdc00592a361602cdbb0c938a35966c1
Ashishkum112 <ashishkumarr0214@gmail.com>
2024-09-06 23:44:21 +0530

    update

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Loading.css'; // Your styling
 
 function Loading() {
+
+  useEffect(() => {
+    // Set visibility to visible once the component has mounted
+    const loadingContainer = document.querySelector('.loading-container');
+    loadingContainer.style.visibility = 'visible';
+  }, []);
+
   return (
-    <div className="loading-container">
+    <div className="loading-container" style={{ visibility: 'hidden' }}>
       <div className="logo-wrapper">
         <img src="/logo2.svg" alt="Logo" className="loading-logo" />
         <div className="loading-spinner"></div>
