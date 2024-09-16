4eec0bad32bbe67238e3a978254506dfba80c061
Ashishkum112 <ashishkumarr0214@gmail.com>
2024-09-16 11:52:45 +0530

    port removed

--- backend/index.js
+++ backend/index.js
@@ -51,7 +51,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: `${process.env.URL || "http://localhost:" + process.env.PORT}/api/v1/user/google/callback`
+    callbackURL: `${process.env.URL}/api/v1/user/google/callback`
 },
 async (accessToken, refreshToken, profile, done) => {
     console.log("profile", profile);
