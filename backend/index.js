7e6eac994b1f273013545653bfb24d7efc715368
Ashishkum112 <ashishkumarr0214@gmail.com>
2024-09-16 11:33:31 +0530

    push

--- backend/index.js
+++ backend/index.js
@@ -51,7 +51,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:8000/api/v1/user/google/callback"
+    callbackURL: `${process.env.URL}/api/v1/user/google/callback`
 },
 async (accessToken, refreshToken, profile, done) => {
     console.log("profile", profile);
